65722c2c633bddc8563f9d50c305a9c5ff9e4f59
Mitch Downey <mitchldowney@gmail.com>
2025-02-01 23:10:22 -0600

    Fix channelCategory relationship

--- src/entities/channel/channelCategory.ts
+++ src/entities/channel/channelCategory.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { Channel } from '@orm/entities/channel/channel';
+import { Category } from '@orm/entities/category';
 
 @Entity('channel_category')
 export class ChannelCategory {
@@ -12,4 +13,8 @@ export class ChannelCategory {
   @ManyToOne(() => Channel, channel => channel.id, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'channel_id' })
   channel!: Channel;
-};
+
+  @ManyToOne(() => Category, category => category.id)
+  @JoinColumn({ name: 'category_id' })
+  category!: Category;
+}
