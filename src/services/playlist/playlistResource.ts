889fba5612c3bcb8f14ebf6742d92b4a61551980
Mitch Downey <mitchldowney@gmail.com>
2025-02-23 11:55:51 -0600

    Reduce duplication in PlaylistResourceService

--- src/services/playlist/playlistResource.ts
+++ src/services/playlist/playlistResource.ts
@@ -1,3 +1,5 @@
+// TODO: get rid of "any" in the file 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { EntityManager, FindOptionsOrderValue } from 'typeorm';
 import { PlaylistResource } from '@orm/entities/playlist/playlistResource';
 import { PlaylistService } from './playlist';
@@ -59,9 +61,11 @@ export class PlaylistResourceService extends BaseManyService<PlaylistResource, '
     return { firstItem, lastItem };
   }
 
-  private async addClipToPlaylist(
+  private async addResourceToPlaylist(
     playlist_id_text: string,
-    clip_id_text: string,
+    resource_id_text: string,
+    resourceService: any,
+    resourceKey: keyof PlaylistResource,
     calculatePosition: (firstItem: PlaylistResource | null, lastItem: PlaylistResource | null) => string
   ): Promise<PlaylistResource> {
     const playlist = await this.playlistService.getByIdText(playlist_id_text);
@@ -69,168 +73,168 @@ export class PlaylistResourceService extends BaseManyService<PlaylistResource, '
       throw new Error("Playlist not found.");
     }
 
-    const clip = await this.clipService.getByIdText(clip_id_text);
-    if (!clip) {
-      throw new Error("Clip not found.");
+    const resource = await resourceService.getByIdText(resource_id_text);
+    if (!resource) {
+      throw new Error(`${resourceKey} not found.`);
     }
 
     const { firstItem, lastItem } = await this.getFirstAndLastItemsByPlaylistIdText(playlist_id_text);
-
     const list_position = calculatePosition(firstItem as PlaylistResource, lastItem as PlaylistResource);
 
     const finalDto = {
-      clip,
+      [resourceKey]: resource,
       list_position
     };
 
     return this._update(
       playlist,
-      ['playlist', 'clip'],
+      ['playlist', resourceKey],
       finalDto
     );
   }
 
-  private async addClipToPlaylistHelper(
+  private async addResourceToPlaylistHelper(
     playlist_id_text: string,
-    clip_id_text: string,
+    resource_id_text: string,
+    resourceService: any,
+    resourceKey: keyof PlaylistResource,
     calculatePosition: (firstItem: PlaylistResource | null, lastItem: PlaylistResource | null) => string
   ): Promise<PlaylistResource> {
-    return this.addClipToPlaylist(playlist_id_text, clip_id_text, calculatePosition);
+    return this.addResourceToPlaylist(playlist_id_text, resource_id_text, resourceService, resourceKey, calculatePosition);
+  }
+
+  private async addResourceToPlaylistFirst(
+    playlist_id_text: string,
+    resource_id_text: string,
+    resourceService: any,
+    resourceKey: keyof PlaylistResource
+  ): Promise<PlaylistResource> {
+    return this.addResourceToPlaylistHelper(playlist_id_text, resource_id_text, resourceService, resourceKey, (firstItem) => {
+      const newPosition = firstItem ? parseFloat(firstItem.list_position) - PLAYLIST_LIST_POSITION_INCREMENT : 1;
+      return newPosition < 0 ? '0' : newPosition.toString();
+    });
+  }
+
+  private async addResourceToPlaylistLast(
+    playlist_id_text: string,
+    resource_id_text: string,
+    resourceService: any,
+    resourceKey: keyof PlaylistResource
+  ): Promise<PlaylistResource> {
+    return this.addResourceToPlaylistHelper(playlist_id_text, resource_id_text, resourceService, resourceKey, (_, lastItem) => {
+      return lastItem ? (parseFloat(lastItem.list_position) + PLAYLIST_LIST_POSITION_INCREMENT).toString() : '1';
+    });
+  }
+
+  private async addResourceToPlaylistBetween(
+    playlist_id_text: string,
+    resource_id_text: string,
+    resourceService: any,
+    resourceKey: keyof PlaylistResource,
+    position1?: number,
+    position2?: number
+  ): Promise<PlaylistResource> {
+    if (position1 === undefined || position2 === undefined) {
+      throw new Error("Both position1 and position2 must be provided.");
+    }
+
+    if (position1 >= position2) {
+      throw new Error("Position1 should be less than Position2.");
+    }
+
+    return this.addResourceToPlaylistHelper(playlist_id_text, resource_id_text, resourceService, resourceKey, () => {
+      const pos1 = parseFloat(position1.toString());
+      const pos2 = parseFloat(position2.toString());
+
+      if (isNaN(pos1) || isNaN(pos2)) {
+        throw new Error("Invalid positions provided.");
+      }
+
+      return ((pos1 + pos2) / 2).toString();
+    });
+  }
+
+  private async removeResourceFromPlaylist(
+    playlist_id_text: string,
+    resource_id_text: string,
+    resourceService: any,
+    resourceKey: keyof PlaylistResource
+  ): Promise<void> {
+    const playlist = await this.playlistService.getByIdText(playlist_id_text);
+    if (!playlist) {
+      throw new Error("Playlist not found.");
+    }
+
+    const resource = await resourceService.getByIdText(resource_id_text);
+    if (!resource) {
+      throw new Error(`${resourceKey} not found.`);
+    }
+
+    return this._delete(playlist, { [`${resourceKey}_id`]: resource.id });
   }
 
   async addClipToPlaylistFirst(playlist_id_text: string, clip_id_text: string): Promise<PlaylistResource> {
-    return this.addClipToPlaylistHelper(playlist_id_text, clip_id_text, (firstItem) => {
-      const newPosition = firstItem ? parseFloat(firstItem.list_position) - PLAYLIST_LIST_POSITION_INCREMENT : 1;
-      return newPosition < 0 ? '0' : newPosition.toString();
-    });
+    return this.addResourceToPlaylistFirst(playlist_id_text, clip_id_text, this.clipService, 'clip');
   }
 
   async addClipToPlaylistLast(playlist_id_text: string, clip_id_text: string): Promise<PlaylistResource> {
-    return this.addClipToPlaylistHelper(playlist_id_text, clip_id_text, (_, lastItem) => {
-      return lastItem ? (parseFloat(lastItem.list_position) + PLAYLIST_LIST_POSITION_INCREMENT).toString() : '1';
-    });
+    return this.addResourceToPlaylistLast(playlist_id_text, clip_id_text, this.clipService, 'clip');
   }
 
   async addClipToPlaylistBetween(playlist_id_text: string, clip_id_text: string, position1?: number, position2?: number): Promise<PlaylistResource> {
-    if (position1 === undefined || position2 === undefined) {
-      throw new Error("Both position1 and position2 must be provided.");
-    }
-
-    if (position1 >= position2) {
-      throw new Error("Position1 should be less than Position2.");
-    }
-
-    return this.addClipToPlaylistHelper(playlist_id_text, clip_id_text, () => {
-      const pos1 = parseFloat(position1.toString());
-      const pos2 = parseFloat(position2.toString());
-
-      if (isNaN(pos1) || isNaN(pos2)) {
-        throw new Error("Invalid positions provided.");
-      }
-
-      return ((pos1 + pos2) / 2).toString();
-    });
+    return this.addResourceToPlaylistBetween(playlist_id_text, clip_id_text, this.clipService, 'clip', position1, position2);
   }
 
   async removeClipFromPlaylist(playlist_id_text: string, clip_id_text: string): Promise<void> {
-    const playlist = await this.playlistService.getByIdText(playlist_id_text);
-    if (!playlist) {
-      throw new Error("Playlist not found.");
-    }
-
-    const clip = await this.clipService.getByIdText(clip_id_text);
-    if (!clip) {
-      throw new Error("Clip not found.");
-    }
-
-    return this._delete(playlist, { clip_id: clip.id });
-  }
-
-  private async addItemToPlaylist(
-    playlist_id_text: string,
-    item_id_text: string,
-    calculatePosition: (firstItem: PlaylistResource | null, lastItem: PlaylistResource | null) => string
-  ): Promise<PlaylistResource> {
-    const playlist = await this.playlistService.getByIdText(playlist_id_text);
-    if (!playlist) {
-      throw new Error("Playlist not found.");
-    }
-
-    const item = await this.itemService.getByIdText(item_id_text);
-    if (!item) {
-      throw new Error("Item not found.");
-    }
-
-    const { firstItem, lastItem } = await this.getFirstAndLastItemsByPlaylistIdText(playlist_id_text);
-
-    const list_position = calculatePosition(firstItem as PlaylistResource, lastItem as PlaylistResource);
-
-    const finalDto = {
-      item,
-      list_position
-    };
-
-    return this._update(
-      playlist,
-      ['playlist', 'item'],
-      finalDto
-    );
-  }
-
-  private async addItemToPlaylistHelper(
-    playlist_id_text: string,
-    item_id_text: string,
-    calculatePosition: (firstItem: PlaylistResource | null, lastItem: PlaylistResource | null) => string
-  ): Promise<PlaylistResource> {
-    return this.addItemToPlaylist(playlist_id_text, item_id_text, calculatePosition);
+    return this.removeResourceFromPlaylist(playlist_id_text, clip_id_text, this.clipService, 'clip');
   }
 
   async addItemToPlaylistFirst(playlist_id_text: string, item_id_text: string): Promise<PlaylistResource> {
-    return this.addItemToPlaylistHelper(playlist_id_text, item_id_text, (firstItem) => {
-      const newPosition = firstItem ? parseFloat(firstItem.list_position) - PLAYLIST_LIST_POSITION_INCREMENT : 1;
-      return newPosition < 0 ? '0' : newPosition.toString();
-    });
+    return this.addResourceToPlaylistFirst(playlist_id_text, item_id_text, this.itemService, 'item');
   }
 
   async addItemToPlaylistLast(playlist_id_text: string, item_id_text: string): Promise<PlaylistResource> {
-    return this.addItemToPlaylistHelper(playlist_id_text, item_id_text, (_, lastItem) => {
-      return lastItem ? (parseFloat(lastItem.list_position) + PLAYLIST_LIST_POSITION_INCREMENT).toString() : '1';
-    });
+    return this.addResourceToPlaylistLast(playlist_id_text, item_id_text, this.itemService, 'item');
   }
 
   async addItemToPlaylistBetween(playlist_id_text: string, item_id_text: string, position1?: number, position2?: number): Promise<PlaylistResource> {
-    if (position1 === undefined || position2 === undefined) {
-      throw new Error("Both position1 and position2 must be provided.");
-    }
-
-    if (position1 >= position2) {
-      throw new Error("Position1 should be less than Position2.");
-    }
-
-    return this.addItemToPlaylistHelper(playlist_id_text, item_id_text, () => {
-      const pos1 = parseFloat(position1.toString());
-      const pos2 = parseFloat(position2.toString());
-
-      if (isNaN(pos1) || isNaN(pos2)) {
-        throw new Error("Invalid positions provided.");
-      }
-
-      return ((pos1 + pos2) / 2).toString();
-    });
+    return this.addResourceToPlaylistBetween(playlist_id_text, item_id_text, this.itemService, 'item', position1, position2);
   }
 
   async removeItemFromPlaylist(playlist_id_text: string, item_id_text: string): Promise<void> {
-    const playlist = await this.playlistService.getByIdText(playlist_id_text);
-    if (!playlist) {
-      throw new Error("Playlist not found.");
-    }
+    return this.removeResourceFromPlaylist(playlist_id_text, item_id_text, this.itemService, 'item');
+  }
 
-    const item = await this.itemService.getByIdText(item_id_text);
-    if (!item) {
-      throw new Error("Item not found.");
-    }
+  async addItemChapterToPlaylistFirst(playlist_id_text: string, item_chapter_id_text: string): Promise<PlaylistResource> {
+    return this.addResourceToPlaylistFirst(playlist_id_text, item_chapter_id_text, this.itemChapterService, 'item_chapter');
+  }
 
-    return this._delete(playlist, { item_id: item.id });
+  async addItemChapterToPlaylistLast(playlist_id_text: string, item_chapter_id_text: string): Promise<PlaylistResource> {
+    return this.addResourceToPlaylistLast(playlist_id_text, item_chapter_id_text, this.itemChapterService, 'item_chapter');
+  }
+
+  async addItemChapterToPlaylistBetween(playlist_id_text: string, item_chapter_id_text: string, position1?: number, position2?: number): Promise<PlaylistResource> {
+    return this.addResourceToPlaylistBetween(playlist_id_text, item_chapter_id_text, this.itemChapterService, 'item_chapter', position1, position2);
+  }
+
+  async removeItemChapterFromPlaylist(playlist_id_text: string, item_chapter_id_text: string): Promise<void> {
+    return this.removeResourceFromPlaylist(playlist_id_text, item_chapter_id_text, this.itemChapterService, 'item_chapter');
+  }
+
+  async addItemSoundbiteToPlaylistFirst(playlist_id_text: string, item_soundbite_id_text: string): Promise<PlaylistResource> {
+    return this.addResourceToPlaylistFirst(playlist_id_text, item_soundbite_id_text, this.itemSoundbiteService, 'item_soundbite');
+  }
+
+  async addItemSoundbiteToPlaylistLast(playlist_id_text: string, item_soundbite_id_text: string): Promise<PlaylistResource> {
+    return this.addResourceToPlaylistLast(playlist_id_text, item_soundbite_id_text, this.itemSoundbiteService, 'item_soundbite');
+  }
+
+  async addItemSoundbiteToPlaylistBetween(playlist_id_text: string, item_soundbite_id_text: string, position1?: number, position2?: number): Promise<PlaylistResource> {
+    return this.addResourceToPlaylistBetween(playlist_id_text, item_soundbite_id_text, this.itemSoundbiteService, 'item_soundbite', position1, position2);
+  }
+
+  async removeItemSoundbiteFromPlaylist(playlist_id_text: string, item_soundbite_id_text: string): Promise<void> {
+    return this.removeResourceFromPlaylist(playlist_id_text, item_soundbite_id_text, this.itemSoundbiteService, 'item_soundbite');
   }
 
   private async addItemAddByRSSToPlaylist(
@@ -310,171 +314,5 @@ export class PlaylistResourceService extends BaseManyService<PlaylistResource, '
     }
 
     return this._delete(playlist, { add_by_rss_hash_id });
-  }
-
-  private async addItemChapterToPlaylist(
-    playlist_id_text: string,
-    item_chapter_id_text: string,
-    calculatePosition: (firstItem: PlaylistResource | null, lastItem: PlaylistResource | null) => string
-  ): Promise<PlaylistResource> {
-    const playlist = await this.playlistService.getByIdText(playlist_id_text);
-    if (!playlist) {
-      throw new Error("Playlist not found.");
-    }
-
-    const itemChapter = await this.itemChapterService.getByIdText(item_chapter_id_text);
-    if (!itemChapter) {
-      throw new Error("Item chapter not found.");
-    }
-
-    const { firstItem, lastItem } = await this.getFirstAndLastItemsByPlaylistIdText(playlist_id_text);
-
-    const list_position = calculatePosition(firstItem as PlaylistResource, lastItem as PlaylistResource);
-
-    const finalDto = {
-      item_chapter: itemChapter,
-      list_position
-    };
-
-    return this._update(
-      playlist,
-      ['playlist', 'item_chapter'],
-      finalDto
-    );
-  }
-
-  private async addItemChapterToPlaylistHelper(
-    playlist_id_text: string,
-    item_chapter_id_text: string,
-    calculatePosition: (firstItem: PlaylistResource | null, lastItem: PlaylistResource | null) => string
-  ): Promise<PlaylistResource> {
-    return this.addItemChapterToPlaylist(playlist_id_text, item_chapter_id_text, calculatePosition);
-  }
-
-  async addItemChapterToPlaylistFirst(playlist_id_text: string, item_chapter_id_text: string): Promise<PlaylistResource> {
-    return this.addItemChapterToPlaylistHelper(playlist_id_text, item_chapter_id_text, (firstItem) => {
-      const newPosition = firstItem ? parseFloat(firstItem.list_position) - PLAYLIST_LIST_POSITION_INCREMENT : 1;
-      return newPosition < 0 ? '0' : newPosition.toString();
-    });
-  }
-
-  async addItemChapterToPlaylistLast(playlist_id_text: string, item_chapter_id_text: string): Promise<PlaylistResource> {
-    return this.addItemChapterToPlaylistHelper(playlist_id_text, item_chapter_id_text, (_, lastItem) => {
-      return lastItem ? (parseFloat(lastItem.list_position) + PLAYLIST_LIST_POSITION_INCREMENT).toString() : '1';
-    });
-  }
-
-  async addItemChapterToPlaylistBetween(playlist_id_text: string, item_chapter_id_text: string, position1?: number, position2?: number): Promise<PlaylistResource> {
-    if (position1 === undefined || position2 === undefined) {
-      throw new Error("Both position1 and position2 must be provided.");
-    }
-
-    if (position1 >= position2) {
-      throw new Error("Position1 should be less than Position2.");
-    }
-
-    return this.addItemChapterToPlaylistHelper(playlist_id_text, item_chapter_id_text, () => {
-      const pos1 = parseFloat(position1.toString());
-      const pos2 = parseFloat(position2.toString());
-
-      if (isNaN(pos1) || isNaN(pos2)) {
-        throw new Error("Invalid positions provided.");
-      }
-
-      return ((pos1 + pos2) / 2).toString();
-    });
-  }
-
-  async removeItemChapterFromPlaylist(playlist_id_text: string, item_chapter_id_text: string): Promise<void> {
-    const playlist = await this.playlistService.getByIdText(playlist_id_text);
-    if (!playlist) {
-      throw new Error("Playlist not found.");
-    }
-
-    const item_chapter = await this.itemChapterService.getByIdText(item_chapter_id_text);
-    if (!item_chapter) {
-      throw new Error("Chapter not found.");
-    }
-
-    return this._delete(playlist, { item_chapter_id: item_chapter.id });
-  }
-
-  private async addItemSoundbiteToPlaylist(
-    playlist_id_text: string,
-    item_soundbite_id_text: string,
-    calculatePosition: (firstItem: PlaylistResource | null, lastItem: PlaylistResource | null) => string
-  ): Promise<PlaylistResource> {
-    const playlist = await this.playlistService.getByIdText(playlist_id_text);
-    if (!playlist) {
-      throw new Error("Playlist not found.");
-    }
-
-    const item_soundbite = await this.itemSoundbiteService.getByIdText(item_soundbite_id_text);
-    if (!item_soundbite) {
-      throw new Error("Soundbite not found.");
-    }
-
-    const { firstItem, lastItem } = await this.getFirstAndLastItemsByPlaylistIdText(playlist_id_text);
-
-    const list_position = calculatePosition(firstItem as PlaylistResource, lastItem as PlaylistResource);
-
-    const finalDto = {
-      item_soundbite,
-      list_position
-    };
-
-    return this._update(
-      playlist,
-      ['playlist', 'item_soundbite'],
-      finalDto
-    );
-  }
-
-  async addItemSoundbiteToPlaylistFirst(playlist_id_text: string, item_soundbite_id_text: string): Promise<PlaylistResource> {
-    return this.addItemSoundbiteToPlaylist(playlist_id_text, item_soundbite_id_text, (firstItem) => {
-      const newPosition = firstItem ? parseFloat(firstItem.list_position) - PLAYLIST_LIST_POSITION_INCREMENT : 1;
-      return newPosition < 0 ? '0' : newPosition.toString();
-    });
-  }
-
-  async addItemSoundbiteToPlaylistLast(playlist_id_text: string, item_soundbite_id_text: string): Promise<PlaylistResource> {
-    return this.addItemSoundbiteToPlaylist(playlist_id_text, item_soundbite_id_text, (_, lastItem) => {
-      return lastItem ? (parseFloat(lastItem.list_position) + PLAYLIST_LIST_POSITION_INCREMENT).toString() : '1';
-    });
-  }
-
-  async addItemSoundbiteToPlaylistBetween(playlist_id_text: string, item_soundbite_id_text: string, position1?: number, position2?: number): Promise<PlaylistResource> {
-    if (position1 === undefined || position2 === undefined) {
-      throw new Error("Both position1 and position2 must be provided.");
-    }
-
-    if (position1 >= position2) {
-      throw new Error("Position1 should be less than Position2.");
-    }
-
-    return this.addItemSoundbiteToPlaylist(playlist_id_text, item_soundbite_id_text, () => {
-      const pos1 = parseFloat(position1.toString());
-      const pos2 = parseFloat(position2.toString());
-
-      if (isNaN(pos1) || isNaN(pos2)) {
-        throw new Error("Invalid positions provided.");
-      }
-
-      return ((pos1 + pos2) / 2).toString();
-    });
-  }
-
-  async removeItemSoundbiteFromPlaylist(playlist_id_text: string, item_soundbite_id_text: string): Promise<void> {
-    const playlist = await this.playlistService.getByIdText(playlist_id_text);
-    if (!playlist) {
-      throw new Error("Playlist not found.");
-    }
-
-    const item_soundbite = await this.itemSoundbiteService.getByIdText(item_soundbite_id_text);
-    if (!item_soundbite) {
-      throw new Error("Soundbite not found.");
-    }
-
-    return this._delete(playlist, { item_soundbite_id: item_soundbite.id });
   }
 }
