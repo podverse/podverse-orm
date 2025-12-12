9a6a2ee632d013dabd5a7e3914e4b7056fa2f1f8
Mitch Downey <mitchldowney@gmail.com>
2025-12-11 23:50:20 -0600

    fix: update LiveItem to use LiveItemStatus instead of LiveItemStatusEnum

--- src/entities/liveItem/liveItem.ts
+++ src/entities/liveItem/liveItem.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToOne } from 'typeorm';
 import { Item } from '@orm/entities/item/item';
-import { LiveItemStatus, LiveItemStatusEnum } from '@orm/entities/liveItem/liveItemStatus';
+import { LiveItemStatus } from '@orm/entities/liveItem/liveItemStatus';
 
 @Entity()
 export class LiveItem {
@@ -13,7 +13,7 @@ export class LiveItem {
 
   @ManyToOne(() => LiveItemStatus, liveItemStatus => liveItemStatus.id)
   @JoinColumn({ name: 'live_item_status_id' })
-  live_item_status!: LiveItemStatusEnum;
+  live_item_status!: LiveItemStatus;
 
   @Column({ name: 'live_item_status_id', type: 'int', nullable: false })
   live_item_status_id!: number;
